2aec70325ebaaee9d9f16ffbe585cb6111c0a11b
Arnaud Lacombe <lacombar@gmail.com>
2011-07-01 19:53:15 -0400

    kconfig/check.sh: prepare for generic check
    `scripts/kconfig/check.sh' currently checks for libintl availability, make this
    explicit. Result gets stored in a ${obj}/.tmp_check Makefile sourced by
    the main Makefile dynamically.
    
    Signed-off-by: Arnaud Lacombe <lacombar@gmail.com>

--- scripts/kconfig/check.sh
+++ scripts/kconfig/check.sh
@@ -1,14 +1,38 @@
 #!/bin/sh
-# Needed for systems without gettext
-$* -xc -o /dev/null - > /dev/null 2>&1 << EOF
-#include <libintl.h>
-int main()
-{
-	gettext("");
-	return 0;
-}
-EOF
-if [ ! "$?" -eq "0"  ]; then
-	echo -DKBUILD_NO_NLS;
-fi
+#
 
+set -e
+
+check_gettext()
+{
+
+	echo '
+	#include <libintl.h>
+	int main()
+	{
+		gettext("");
+		return 0;
+	}' | \
+	{
+		$* -xc -o /dev/null - > /dev/null 2>&1 || \
+			echo HOST_EXTRACFLAGS	+= -DKBUILD_NO_NLS >> ${obj}/.tmp_check
+	}
+}
+
+rm -f ${obj}/.tmp_check
+
+for arg in $*; do
+	case $arg in
+	gettext)	;;
+	*)
+		echo "  *"
+		echo "  * Do not know how to check for \`$arg'"
+		echo "  *"
+		false
+		;;
+	esac
+	echo "  CHECK   $arg"
+
+	check_$arg
+	echo "KCONFIG_CHECKED_$arg := 1"	>> ${obj}/.tmp_check
+done
